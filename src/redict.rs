87c32aaaf4ffd7851427a118590468ea7e20be41
Alfred Dobradi <alfreddobradi@gmail.com>
2025-02-27 19:39:51 +0000

    feat: Add and update routes - no auth

--- src/redict.rs
+++ src/redict.rs
@@ -1,8 +1,9 @@
-use redis::{Commands, Connection as RedictConnection, ErrorKind, FromRedisValue, RedisError, RedisResult, Value};
-use redis::ErrorKind::TypeError;
-use redis::Value::Map;
-use tracing::info;
-use crate::api;
+use crate::api::error::ApiError;
+use axum::http::StatusCode;
+use redis::{
+    Commands, Connection as RedictConnection, ErrorKind, FromRedisValue, RedisError, RedisResult,
+    Value,
+};
 
 pub type UrlList = Vec<String>;
 
@@ -14,18 +15,18 @@ pub struct UrlItem {
 
 impl From<(&Value, &Value)> for UrlItem {
     fn from((raw_url, raw_hits): (&Value, &Value)) -> Self {
-        let url: String;
-        let hits: u64;
+        let url: String = match raw_url {
+            Value::BulkString(u) => String::from_utf8(u.clone()).unwrap_or_default(),
+            _ => "".to_string(),
+        };
 
-        match raw_url {
-            Value::BulkString(u) => url = String::from_utf8(u.clone()).unwrap_or_default(),
-            _ => url = "".to_string(),
-        }
-
-        match raw_hits {
-            Value::BulkString(h) => hits = String::from_utf8(h.clone()).unwrap_or_default().parse::<u64>().unwrap_or_default(),
-            _ => hits = 0,
-        }
+        let hits = match raw_hits {
+            Value::BulkString(h) => String::from_utf8(h.clone())
+                .unwrap_or_default()
+                .parse::<u64>()
+                .unwrap_or_default(),
+            _ => 0,
+        };
 
         Self { url, hits }
     }
@@ -38,27 +39,26 @@ impl FromRedisValue for UrlItem {
                 let mut url_raw = &Value::BulkString(Vec::new());
                 let mut hits_raw = &Value::BulkString(Vec::new());
 
-                let mut index = 0;
-                for v in array {
+                for (index, v) in array.iter().enumerate() {
                     if let Value::BulkString(val) = v {
                         let value = String::from_utf8(val.to_vec())?;
                         match value.as_str() {
-                            "url" => url_raw = array.get(index+1).unwrap_or(&Value::Nil),
-                            "hits" => hits_raw = array.get(index+1).unwrap_or(&Value::Nil),
+                            "url" => url_raw = array.get(index + 1).unwrap_or(&Value::Nil),
+                            "hits" => hits_raw = array.get(index + 1).unwrap_or(&Value::Nil),
                             _ => {}
                         }
                     };
-
-                    index += 1;
                 }
 
                 Ok(Self::from((url_raw, hits_raw)))
             }
-            _ => Err(RedisError::from((TypeError, "Failed to parse redict value")))
+            _ => Err(RedisError::from((
+                ErrorKind::TypeError,
+                "Failed to parse redict value",
+            ))),
         }
     }
 }
-
 
 pub struct Connection {
     c: RedictConnection,
@@ -72,14 +72,14 @@ impl From<(RedictConnection, String)> for Connection {
 }
 
 impl Connection {
-    pub fn get_list(&mut self) -> Result<Vec<String>, api::error::ApiError> {
+    pub fn get_list(&mut self) -> Result<Vec<String>, ApiError> {
         let list_key = format!("url:{}:list", self.version);
         let list: Vec<String> = self.c.smembers(list_key)?;
 
         Ok(list)
     }
 
-    pub fn get_item(&mut self, key: &String) -> Result<UrlItem, api::error::ApiError> {
+    pub fn get_item(&mut self, key: &String) -> Result<UrlItem, ApiError> {
         let item_key = format!("url:{}:{}", self.version, key);
 
         let item: UrlItem = self.c.hgetall(item_key)?;
@@ -87,17 +87,30 @@ impl Connection {
         Ok(item)
     }
 
-    pub fn add_item(&mut self, key: String, value: String) -> Result<UrlItem, api::error::ApiError> {
+    pub fn add_item(&mut self, key: &String, value: String, force: bool) -> Result<(), ApiError> {
         let list_key = format!("url:{}:list", self.version);
-        let item = UrlItem{url: value, hits: 0};
-
         let item_key = format!("url:{}:{}", self.version, key);
 
-        self.c.sadd(list_key, item_key)?;
+        self.c
+            .sadd::<String, String, ()>(list_key.clone(), item_key.clone())?;
 
+        if !force
+            && self
+                .c
+                .hget::<String, String, ()>(item_key.clone(), "url".to_string())
+                .is_ok()
+        {
+            return Err(ApiError::StatusCode(StatusCode::CONFLICT));
+        }
 
-        Ok(item)
+        self.c.hset_multiple::<String, String, String, ()>(
+            item_key,
+            &[
+                ("url".to_string(), value),
+                ("hits".to_string(), "0".to_string()),
+            ],
+        )?;
+
+        Ok(())
     }
 }
-
-
