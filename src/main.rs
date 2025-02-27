3f8ea484fc3cf02503b8c0fc213d435e2cbbfcc4
Alfred Dobradi <alfreddobradi@gmail.com>
2025-02-27 00:57:40 +0000

    fix: Kubernetes fixes, bind to 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ async fn main() -> Result<(), anyhow::Error> {
         .route("/{url_key}", get(api::handlers::handle_redirect))
         .with_state(state);
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     info!("listening on {}", listener.local_addr()?);
     axum::serve(listener, app).await?;
 
